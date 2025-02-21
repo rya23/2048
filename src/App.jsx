50ffe60c83cd6cf049b65446cda0f288a2f886fc
rya23 <thakur.aarya23@gmail.com>
2025-02-21 22:52:39 +0530

    vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-import React, { useState } from "react"
-import GameBoard from "./Board.jsx"
+import React, { useState } from 'react';
+import GameBoard from './Board.jsx';
+import { Analytics } from '@vercel/analytics/react';
 
 const App = () => {
     return (
         <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
             <GameBoard />
+            <Analytics />
         </div>
-    )
-}
+    );
+};
 
-export default App
+export default App;
